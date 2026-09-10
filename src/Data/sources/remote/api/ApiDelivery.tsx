07d085d6795d1317694ec29b086d9f73f0459395
Katherine <kathearias429@gmail.com>
2026-09-10 17:38:09 -0500

    chore: actualizacion de SDK de 54 a 57

--- src/Data/sources/remote/api/ApiDelivery.tsx
+++ src/Data/sources/remote/api/ApiDelivery.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const ApiDelivery = axios.create({
-    baseURL: 'http:// 192.168.2.80:3001/api',   headers: {
+    baseURL: 'http://172.20.10.18:3001/api',   headers: {
         'Content-Type': 'application/json'
     }
 });
