a2c77ff8811e6640c7edc135a1ed2ce735502a4d
Juanes115 <juangalindoestebandiaz@gmail.com>
2026-09-15 12:23:19 -0500

    refactor: eliminar arquitectura Clean Architecture reemplazada por nuevas screens

--- src/Domain/UseCases/userLocal/GetUserLocal.tsx
+++
@@ -1,7 +0,0 @@
-import { UserLocalRepositoryImp } from "../../../Data/repositories/UserLocalRepository";
-
-const { getUser } = new UserLocalRepositoryImp();
-
-export const GetUserLocalUseCase = async() => {
-   return await getUser();
-}
--- src/Domain/UseCases/userLocal/RemoveUserLocal.tsx
+++
@@ -1,9 +0,0 @@
-import { UserLocalRepositoryImp } from "../../../Data/repositories/UserLocalRepository";
-import { User } from "../../entities/User";
-
-const { remove } = new UserLocalRepositoryImp();
-
-export const RemoveUserLocalUseCase = async() => {
-   return await remove();
-}   
-
